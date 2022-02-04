add81c0f196ea193a502299ff5c9a33f0d81926c
tea-mochi <56103047+tea-mochi@users.noreply.github.com>
2022-02-04 15:39:43 -0800

    fix failing test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -8,6 +8,6 @@ java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnit
 public class SkillDemoTest {
     @Test
     public void testMultiply(){
-        assertEquals(42, SkillDemo.multiply(8,5)); //this fails
+        assertEquals(42, SkillDemo.multiply(2,21)); //this now passes
     }
 }
